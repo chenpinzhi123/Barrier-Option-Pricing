Tiny changes: 1. Control Monte Carlo needs fewer samples (decreasing default MC_lens) 2. Fixed errors in delta calculation 3. Added testing methods for static-dynamic hedging (to be done later)

Options/Classes.py
@@ -315,11 +315,11 @@ class Vanilla:
         在默认情况下，假设MC1_lens = MC_lens
         """
         if MC1_lens == None:
-            MC1_lens = MC_lens
+            MC1_lens = int(MC_lens / 10)
         
         mu = self.r - self.q
         
-        SAll = self.MonteCarloGenerate(St, filename, MC_lens, T_lens, MCMethod)
+        SAll = self.MonteCarloGenerate(St, filename, MC1_lens, T_lens, MCMethod)
 
         Optionprice = self.MCSolver(SAll, )
         Matcov = np.cov(Optionprice['OptionPrice'], Optionprice['LastPrice'])
@@ -327,7 +327,7 @@ class Vanilla:
         c = -Matcov[0, 1] / Var_s
         Exp_s = St * np.exp(mu * self.t)
 
-        SAll_con = self.MonteCarloGenerate(St, filename, MC1_lens, T_lens, MCMethod)
+        SAll_con = self.MonteCarloGenerate(St, filename, MC_lens, T_lens, MCMethod)
         Optionprice_con = self.MCSolver(SAll_con, )
         Control_price = Optionprice_con['OptionPrice'] + c * (Optionprice_con['LastPrice'] - Exp_s)
         

Options/Hedge.py
@@ -66,14 +66,14 @@ def DeltaHedgeMonteCarlo(OptionClass, MC_lens, T_lens, **kwargs):
                     # Triggered
                     trigIndex = trigL[0]
                     c_paths[i, trigIndex:] = rebate * np.exp(-r*t_matrix[i, trigIndex:])
-                    c_delta[i, trigIndex:] = rebate * np.exp(-r*t_matrix[i, trigIndex:])
+                    c_delta[i, trigIndex:] = 0
             if OptionClass.barrier == "uo":
                 trigL = np.where(SAll[i,:] > h)[0]
                 if len(trigL) > 0:
                     # Triggered
                     trigIndex = trigL[0]
                     c_paths[i, trigIndex:] = rebate * np.exp(-r*t_matrix[i, trigIndex:])
-                    c_delta[i, trigIndex:] = rebate * np.exp(-r*t_matrix[i, trigIndex:])
+                    c_delta[i, trigIndex:] = 0
             if OptionClass.barrier == "di":
                 trigL = np.where(SAll[i,:] < h)[0]
                 if len(trigL) > 0:
@@ -103,3 +103,14 @@ def DeltaHedgeMonteCarlo(OptionClass, MC_lens, T_lens, **kwargs):
     pnl = dS_sum - dc_sum 
     
     return pnl
+
+def StaticDynamicDeltaHedgeMonteCarlo(OptionClass, MC_lens, T_lens, **kwargs):
+    """
+    Static:
+        Carr and Chou 1997
+    Dynamic:
+        Not optimal Ilhan and Sircar 2006.
+        Instead, more simple delta hedging on the unhedged deltas except from
+        static hedging portfolio
+    """
+    raise NotImplementedError